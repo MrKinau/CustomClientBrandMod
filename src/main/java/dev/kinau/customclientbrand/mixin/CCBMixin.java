f8962509969af99a750fb775c9b83b22127db568
David Luedtke <david@ldtke.de>
2023-09-25 11:04:04 +0200

    1.20.2

--- src/main/java/dev/kinau/customclientbrand/mixin/CCBMixin.java
+++ src/main/java/dev/kinau/customclientbrand/mixin/CCBMixin.java
@@ -3,7 +3,8 @@ package dev.kinau.customclientbrand.mixin;
 import dev.kinau.customclientbrand.config.CCBConfig;
 import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.network.protocol.game.ServerboundCustomPayloadPacket;
+import net.minecraft.network.protocol.common.ServerboundCustomPayloadPacket;
+import net.minecraft.network.protocol.common.custom.BrandPayload;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,8 +15,8 @@ public class CCBMixin {
 
 	@Inject(method = "write(Lnet/minecraft/network/FriendlyByteBuf;)V", at = @At(value = "HEAD"), cancellable = true)
 	public void changeClientBrand(FriendlyByteBuf buf, CallbackInfo ci) {
-		if (((ServerboundCustomPayloadPacket)(Object)this).getIdentifier() == ServerboundCustomPayloadPacket.BRAND) {
-			buf.writeResourceLocation(ServerboundCustomPayloadPacket.BRAND);
+		if (((ServerboundCustomPayloadPacket)(Object)this).payload().id() == BrandPayload.ID) {
+			buf.writeResourceLocation(BrandPayload.ID);
 			buf.writeUtf(AutoConfig.getConfigHolder(CCBConfig.class).get().customBrand());
 			ci.cancel();
 		}
